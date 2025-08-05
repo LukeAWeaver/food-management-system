8a066c735d55f8476184ac9ca246e7b300f9400d
LukeAWeaver <zambusto@gmail.com>
2025-08-04 23:54:46 -0500

    food form functionality + snack/ingredient list rendering

--- backend/app.module.ts
+++ backend/app.module.ts
@@ -1,16 +1,20 @@
 import { Module } from '@nestjs/common'
 import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
 import { FoodItemResolver } from 'src/food-item/food-item.resolver'
 import { FoodItemService } from 'src/food-item/food-item.service'
-
+import { PrismaService } from 'prisma/prisma.service'
+import { PrismaModule } from 'prisma/prisma.module'
 
 @Module({
   imports: [
-    GraphQLModule.forRoot({
+    PrismaModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [FoodItemResolver, FoodItemService],
+  providers: [FoodItemResolver, FoodItemService, PrismaService],
 })
 export class AppModule {}
